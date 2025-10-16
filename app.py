5abbaebe565904be0f27c2868de550648ca0d5d4
Usman Turajo <ukturajo@gmail.com>
2025-10-16 11:38:03 -0700

    Initial commit

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
 from flask import Flask, request, jsonify
-import pickle
+from flask import Flask, request, jsonify
+import joblib
 import numpy as np
-
-model = pickle.load(open('model.pkl', 'rb'))
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Gradient Boosting Housing Price API is running!"
+# Load model and scaler
+model = joblib.load('housing_price_model.pkl')
+scaler = joblib.load('scaler.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json(force=True)
-    features = np.array([list(data.values())])
-    prediction = model.predict(features)
-    return jsonify({'predicted_price': float(prediction[0])})
+    
+    # Extract features in correct order
+    features = np.array([[
+        data['area'], data['bedrooms'], data['bathrooms'], data['stories'],
+        data['mainroad'], data['guestroom'], data['basement'],
+        data['hotwaterheating'], data['airconditioning'], data['parking'],
+        data['prefarea'], data['furnishingstatus_semi-furnished'],
+        data['furnishingstatus_unfurnished']
+    ]])
+    
+    # Scale features
+    features_scaled = scaler.transform(features)
+    
+    # Predict
+    prediction = model.predict(features_scaled)[0]
+    
+    return jsonify({'predicted_price': round(prediction, 2)})
 
 if __name__ == '__main__':
     app.run(debug=True)
